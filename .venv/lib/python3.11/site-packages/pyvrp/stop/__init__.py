afd1dcb14dc5e83ad57d1bbbcbc822304a4af599
zrtgzrtg <thomas4stank@gmail.com>
2025-04-28 21:07:53 +0200

    Reapplied .gitignore

--- .venv/lib/python3.11/site-packages/pyvrp/stop/__init__.py
+++
@@ -1,6 +0,0 @@
-from .FirstFeasible import FirstFeasible as FirstFeasible
-from .MaxIterations import MaxIterations as MaxIterations
-from .MaxRuntime import MaxRuntime as MaxRuntime
-from .MultipleCriteria import MultipleCriteria as MultipleCriteria
-from .NoImprovement import NoImprovement as NoImprovement
-from .StoppingCriterion import StoppingCriterion as StoppingCriterion
